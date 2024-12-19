f769fbd06345b85800b7aa2936080f93953e4252
AlexanderChao14 <chaoalexander45@gmail.com>
2024-12-18 17:54:45 -0700

    Basic Table view for database
    - Added a basic table view for the employee timesheet data, containing the required data fields.

--- frontend/clock-in-app/src/components/Content.svelte
+++ frontend/clock-in-app/src/components/Content.svelte
@@ -1,6 +1,6 @@
 <script>
     import { Tabs } from '@skeletonlabs/skeleton-svelte';
-    let group = $state('Input')
+    let group = $state('Database')
 
     let firstName = $state('');
     let lastName = $state('');
@@ -13,10 +13,16 @@
     function checkIsNumber(){
         id = id.replace(/[^0-9]/g,'')
     }
+
+    const employeeDB = [
+        {id:'123456', firstName: "Mike", lastName: "Smith", clockInTime: "1997-07-16T19:20:30.45+01:00", clockOutTime:"1997-07-16T19:21:30.45+01:00"},
+        {id:'222222', firstName: "Chris", lastName: "John", clockInTime: "1997-08-16T19:20:30.45+01:00", clockOutTime:"1997-09-16T19:23:30.45+01:00"},
+        {id:'123456', firstName: "Mary", lastName: "Jean", clockInTime: "1997-07-16T19:22:30.45+01:00", clockOutTime:null}
+    ]
     
 </script>
 
-<div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 w-2/3 h-full p-4 text-center py-6 justify-self-center">
+<div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 w-5/6 h-full p-4 text-center py-6 justify-self-center">
 
     <Tabs bind:value={group} fluid>
         {#snippet list()}
@@ -50,7 +56,35 @@
                     </div>
                 </div>
             </Tabs.Panel>
-        <Tabs.Panel value="Database">Database things </Tabs.Panel>
+        <Tabs.Panel value="Database">
+            <div class="justify-self-center max-w-auto">
+                <table class="table table-auto border-collapse caption-top">
+                    <caption>
+                        Employee Timesheet Database
+                      </caption>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Clock-In Time</th>
+                            <th>Clock-Out Time</th>
+                        </tr>
+                    </thead>
+                    <tbody class="hover:[&>tr]:preset-tonal-primary">
+                        {#each employeeDB as item}
+                                <tr>
+                                    <td class="text-xs md:text-base text-wrap">{item.id}</td>
+                                    <td class="text-xs md:text-base text-wrap">{item.firstName}</td>
+                                    <td class="text-xs md:text-base text-wrap">{item.lastName}</td>
+                                    <td class="text-xs md:text-base text-wrap">{item.clockInTime}</td>
+                                    <td class="text-xs md:text-base text-wrap">{item.clockOutTime}</td>
+                                </tr>
+                        {/each}      
+                    </tbody>
+                </table>
+            </div>    
+        </Tabs.Panel>
         {/snippet}
     </Tabs>
 </div>
@@ -58,5 +92,8 @@
 <style>
     .input::-webkit-inner-spin-button{
         -webkit-appearance: none;
+    }
+    .td{
+        font-size: 11px;
     }
 </style>
